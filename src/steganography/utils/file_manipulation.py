3f74eb32c3baf4aa963f01c2d55e54126da58b66
Samuel Huwiler <samuel.huwiler@gmx.ch>
2023-12-27 15:28:50 +0100

    Docstrings

--- src/steganography/utils/file_manipulation.py
+++ src/steganography/utils/file_manipulation.py
@@ -3,15 +3,18 @@ from bitlist import bitlist
 
 
 def read_bits_from_file(filename: str) -> bitlist:
+    """read the bits from a given file in memory"""
     with open(filename, "rb") as f:
         return bitlist(f.read())
 
 
 def build_file_in_memory_from_bits(content: bitlist) -> io.BytesIO:
+    """create a BytesIO object from a given array of bits"""
     file = io.BytesIO(content.to_bytes())
     return file
 
 
 def write_file_to_disk(file: io.BytesIO, filename: str) -> None:
+    """Write a BytesIO file to disk"""
     with open(filename, "wb") as f:
         f.write(file.getbuffer())
