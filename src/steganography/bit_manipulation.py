a24dec1c5b022235643befff5d35ca72c06cd7d7
Samuel Huwiler <samuel.huwiler@gmx.ch>
2023-12-27 14:34:52 +0100

    Added set_n_LSB method and changed the set_bit method

--- src/steganography/bit_manipulation.py
+++ src/steganography/bit_manipulation.py
@@ -1,7 +1,25 @@
-def set_bit(org: int, n: int, new: int) -> int:
-    """set the n'th bit of org to new (copied from: https://github.com/livz/cloacked-pixel/blob/master/lsb.py#L43)"""
-    mask = 1 << n
-    org &= ~mask
-    if new:
-        n |= mask
-    return org
+from typing import Union
+from collections.abc import Sequence
+
+
+def set_bit(v: int, index: int, x: Union[int, bool]) -> int:
+    """Set the index:th bit of v to 1 if x is truthy, else to 0, and return the new value."""
+    # copied from: https://stackoverflow.com/questions/12173774/how-to-modify-bits-in-an-integer
+    if isinstance(x, int) and x not in (0, 1):
+        raise ValueError("x must be 1 or 0")
+
+    mask = 1 << index  # Compute mask, an integer with just bit 'index' set.
+    v |= mask  # Set the bit indicated by the mask to True.
+    v ^= (
+        not x
+    ) * mask  # If x is True, do nothing (XOR with 0). If x is False, use the mask for clearing the bit indicated by the mask (XOR with 1 in the requested position).
+    return v
+
+
+def set_n_LSB(v: int, n: int, x: Sequence[Union[int, bool]]) -> int:
+    """set the n LSB to the corresponding vallue in x"""
+    if len(x) != n:
+        raise ValueError("x must contain n elements")
+    for i in range(n):
+        v = set_bit(v, i, x[i])
+    return v
---
+++ test/test_bit_manipulation.py
@@ -0,0 +1,39 @@
+import pytest
+from steganography.bit_manipulation import set_bit, set_n_LSB
+
+
+def test_set_bit() -> None:
+    assert set_bit(255, 0, 0) == 254
+    assert set_bit(255, 1, 0) == 253
+    assert set_bit(171, 0, 0) == 170
+    assert set_bit(171, 5, 0) == 139
+    assert set_bit(10, 3, 0) == 2
+    assert set_bit(2, 0, 1) == 3
+    assert set_bit(0, 0, 1) == 1
+
+    assert set_bit(255, 0, False) == 254
+    assert set_bit(255, 1, False) == 253
+    assert set_bit(171, 0, False) == 170
+    assert set_bit(171, 5, False) == 139
+    assert set_bit(10, 3, False) == 2
+    assert set_bit(2, 0, True) == 3
+    assert set_bit(0, 0, True) == 1
+
+    with pytest.raises(ValueError):
+        set_bit(255, 0, 3)
+        set_bit(255, 0, 3)
+        set_bit(255, 0, -1000)
+        set_bit(255, 0, -1)
+
+
+def test_set_n_LSB() -> None:
+    assert set_n_LSB(255, 1, (0,)) == 254
+    assert set_n_LSB(255, 1, [0]) == 254
+
+    assert set_n_LSB(255, 2, (0, 0)) == 252
+    assert set_n_LSB(255, 3, (0, 1, 0)) == 250
+    with pytest.raises(ValueError):
+        set_n_LSB(255, 4, (0,))
+        set_n_LSB(255, 4, (0, 1))
+        set_n_LSB(255, 4, (0, 1, 1))
+        set_n_LSB(255, 4, (0, 1, 0, 1, 0))
