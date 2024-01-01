ea09f7055b34a92b4007a4f972016d178cb17217
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-01-01 22:01:39 +0100

    Updates

--- src/steganography/utils/encryption.py
+++ src/steganography/utils/encryption.py
@@ -1,10 +1,3 @@
-import base64
-from Crypto.Cipher import AES
-from Crypto.Hash import SHA256
-from Crypto import Random
-from typing import Union
-
-
 def encrypt(key: str, source: bytes) -> bytes:
     return NotImplemented
 
