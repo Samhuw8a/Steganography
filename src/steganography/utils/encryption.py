ea016ef6e6a409a5cf7bc3fb24b10d7535a80a08
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-01-01 22:13:33 +0100

    Oops package has another name for versions > 3.3

--- src/steganography/utils/encryption.py
+++ src/steganography/utils/encryption.py
@@ -1,14 +1,17 @@
+# Copied from https://stackoverflow.com/questions/42568262/how-to-encrypt-text-with-a-password-in-python
+
 import base64
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto import Random
+from typing import Union
+
+StringOrBytes = Union[str, bytes]
 
 
-def encrypt(key, source, encode=True):
-    key = SHA256.new(
-        key
-    ).digest()  # use SHA-256 over our key to get a proper-sized AES key
-    IV = Random.new().read(AES.block_size)  # generate IV
+def encrypt(key: StringOrBytes, source: bytes, encode: bool = True) -> StringOrBytes:
+    key = SHA256.new(key).digest()  # use SHA-256 to get a proper-sized AES key
+    IV: bytes = Random.new().read(AES.block_size)  # type: ignore
     encryptor = AES.new(key, AES.MODE_CBC, IV)
     padding = AES.block_size - len(source) % AES.block_size  # calculate needed padding
     source += bytes([padding]) * padding  # Python 2.x: source += chr(padding) * padding
@@ -16,18 +19,16 @@ def encrypt(key, source, encode=True):
     return base64.b64encode(data).decode("latin-1") if encode else data
 
 
-def decrypt(key, source, decode=True):
+def decrypt(
+    key: StringOrBytes, rsource: StringOrBytes, decode: bool = True
+) -> StringOrBytes:
     if decode:
-        source = base64.b64decode(source.encode("latin-1"))
-    key = SHA256.new(
-        key
-    ).digest()  # use SHA-256 over our key to get a proper-sized AES key
+        source = base64.b64decode(rsource.encode("latin-1"))  # type: ignore
+    key = SHA256.new(key).digest()  # use SHA-256 to get a proper-sized AES key
     IV = source[: AES.block_size]  # extract the IV from the beginning
     decryptor = AES.new(key, AES.MODE_CBC, IV)
     data = decryptor.decrypt(source[AES.block_size :])  # decrypt
     padding = data[-1]  # pick the padding value from the end; Python 2.x: ord(data[-1])
-    if (
-        data[-padding:] != bytes([padding]) * padding
-    ):  # Python 2.x: chr(padding) * padding
+    if data[-padding:] != bytes([padding]) * padding:
         raise ValueError("Invalid padding...")
     return data[:-padding]  # remove the padding
