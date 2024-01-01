2f0c1e49899ec32353fe2117f8bb0b5686a6c8d6
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-01-01 22:22:36 +0100

    Tests for encryption

--- test/test_encryption.py
+++ test/test_encryption.py
@@ -1,1 +1,16 @@
 from steganography.utils.encryption import encrypt, decrypt
+from hypothesis import given
+from hypothesis.strategies import binary
+
+
+@given(data=binary(), key=binary())
+def test_encrypt_decrypt_reversal(data: bytes, key: bytes) -> None:
+    assert data == decrypt(key, (encrypt(key, data)))
+
+
+@given(data=binary(), key=binary())
+def test_encryption_randomness(data, key) -> None:
+    e1 = encrypt(key, data)
+    e2 = encrypt(key, data)
+    assert e1 != e2
+    assert decrypt(key, e1) == decrypt(key, e2)
