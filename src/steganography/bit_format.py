030759c67f181a1f9678fd9ba169cf0c7014eac2
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-01-01 21:10:03 +0100

    parameterized lenght bits

--- src/steganography/bit_format.py
+++ src/steganography/bit_format.py
@@ -2,14 +2,13 @@ from numpy.typing import NDArray
 
 __all__ = ["get_max_payload_size"]
 
-LENGHT = 20  # Laenge der Payload in Bits
 HASH = 256  # Länge des SHA-256 Hash in bits
 
 
-def get_max_payload_size(pixels: NDArray, nlsb: int) -> int:
+def get_max_payload_size(pixels: NDArray, nlsb: int, lenght_bits: int = 20) -> int:
     """Max size of payload in bits"""
     vals = len(pixels) - 1  # Anzahl an Freien Pixel minus 1 für das LSB pixel
     available = vals * nlsb  # Anzahl Freier Bits nach LSB bits
-    available -= LENGHT
+    available -= lenght_bits
     available -= HASH
     return available
