6b3cc0567aebceeea16add8d8bb5835e6a205b0e
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-01-08 20:57:14 +0100

    Moved file_compression method to bit_format and implementing build_bits_for_file function according to bit_encoding_format.md

--- src/steganography/bit_format.py
+++ src/steganography/bit_format.py
@@ -1,22 +1,19 @@
 from __future__ import annotations
+import lzma
 from numpy.typing import NDArray
 from bitlist import bitlist
 from steganography.utils.misc import HashFunction
-from typing import Tuple
+from steganography.utils.encryption import encrypt
+from typing import Tuple, Union
 
 
-__all__ = [
-    "build_bits_for_file",
-    "extract_file_and_metadata_from_raw_bits",
-    "add_seperator_tag_to_file",
-    "get_max_payload_size",
-]
+__all__ = ["build_bits_for_file", "extract_file_and_metadata_from_raw_bits", "STEG_TAG"]
 
 HASH = 256  # Länge des SHA-256 Hash in bits
 STEG_TAG = bytes(b"[STEG]")  # Steg Tag zum Abgränzen der versteckten Datei
 
 
-def get_max_payload_size(pixels: NDArray, nlsb: int, tag: bytes = STEG_TAG) -> int:
+def _get_max_payload_size(pixels: NDArray, nlsb: int, tag: bytes = STEG_TAG) -> int:
     """Max size of payload in bits"""
     vals = len(pixels) - 1  # Anzahl an Freien Pixel minus 1 für das LSB pixel
     available = vals * nlsb  # Anzahl Freier Bits nach LSB bits
@@ -27,13 +24,21 @@ def get_max_payload_size(pixels: NDArray, nlsb: int, tag: bytes = STEG_TAG) -> i
     return available
 
 
-def add_seperator_tag_to_file(file: bytes, tag: bytes) -> bytes:
+def _add_seperator_tag_to_file(file: Union[str, bytes], tag: bytes) -> bytes:
     """Returns file bytes with added Tag; 'tag file tag'"""
-    return tag + file + tag
+    if isinstance(file, bytes):
+        bfile = file
+    else:
+        bfile = file.encode()
+    return tag + bfile + tag
+
+
+def _compress_file(file: bytes) -> bytes:
+    return lzma.compress(file)
 
 
 def build_bits_for_file(
-    file_content: bytes, file_name: str, hash_func: HashFunction
+    file_content: bytes, file_name: str, hash_func: HashFunction, encryption_key: bytes
 ) -> bitlist:
     """
     Build to corect bits for embeding into the pixels. The NLSB pixel has to be encoded seperate.
@@ -42,7 +47,15 @@ def build_bits_for_file(
     :param hash_func: function for hashing the file bytes
     :return: bits for embeding
     """
-    return NotImplemented
+    file_hash = bitlist.fromhex(hash_func(file_content))
+    compressed = _compress_file(file_content)
+    ecrypted = encrypt(encryption_key, compressed, False)
+    with_tag = _add_seperator_tag_to_file(ecrypted, STEG_TAG)
+    file = bitlist(with_tag)
+    filename_bits = bitlist(bytes(file_name.encode()))
+    bits = file_hash.bin() + filename_bits.bin() + file.bin()
+    print(bits)
+    return bitlist(bits)
 
 
 def extract_file_and_metadata_from_raw_bits(
