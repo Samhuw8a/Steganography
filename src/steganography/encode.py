5e29d095c38d988a8f984aa93e7d1e8ed53af8b6
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-01-11 12:28:19 +0100

    Added basic _encode_file_in_pixels function

--- src/steganography/encode.py
+++ src/steganography/encode.py
@@ -6,7 +6,12 @@ from numpy.typing import NDArray
 from PIL.Image import Image as Img
 
 from steganography.utils.misc import ImageModeException, ImageTypeException
-from steganography.utils.pixel_manipulation import seperate_rgb_and_alpha
+from steganography.utils.pixel_manipulation import (
+    seperate_rgb_and_alpha,
+    embed_bits_in_pixels,
+)
+from steganography.utils.hashing import hash_file
+from steganography.bit_format import STEG_TAG, build_bits_for_file, get_max_payload_size
 
 __all__ = ["encode_file_in_image"]
 
@@ -31,6 +36,25 @@ def _encode_image_to_rgb_and_alpha_array(image: Img) -> Tuple[NDArray, NDArray]:
     pixels = np.array(image).flatten()
     rgb, alpha = seperate_rgb_and_alpha(pixels)
     return (rgb, alpha)
+
+
+def _encode_file_in_pixels(
+    pixels: NDArray,
+    file_content: bytes,
+    file_name: str,
+    encryption_key: bytes,
+    n_lsb: int,
+) -> NDArray:
+    """encode the file and Metadata into a flattened rgb-array"""
+    max_size = get_max_payload_size(pixels, n_lsb, tag=STEG_TAG)
+    file_size = len(file_content) * 8 + len(file_name.encode()) * 8
+    if file_size > max_size:
+        raise ValueError("the File and filename are too big to store into the Image")
+    bits = build_bits_for_file(
+        file_content, file_name, hash_file, encryption_key, steg_tag=STEG_TAG
+    )
+    embeded_pixels = embed_bits_in_pixels(pixels, bits, n_lsb)
+    return embeded_pixels
 
 
 def encode_file_in_image(file: IO[bytes], image: Img, n_lsb: int) -> Img:
