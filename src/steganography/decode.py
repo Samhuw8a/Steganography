8cafcc436c03af123e9c6522aeb903fe2eb8aa4c
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-01-19 15:16:45 +0100

    Implemented MVP for decoding

--- src/steganography/decode.py
+++ src/steganography/decode.py
@@ -8,6 +8,7 @@ from bitlist import bitlist
 from steganography.utils.pixel_manipulation import encode_image_to_rgb_and_alpha_array
 from steganography.bit_format import extract_file_and_metadata_from_raw_bits
 from steganography.utils.misc import DEFAULT_ENCRYPTION_KEY
+from steganography.utils.hashing import validate_hash, hash_file
 
 
 def _get_n_lsb_from_list_of_bitlists(bits: List[bitlist], n_lsb: int) -> bitlist:
@@ -30,13 +31,26 @@ def _decode_bits_from_pixels(pixels: NDArray) -> Tuple[int, bitlist]:
 
 def decode_file_from_image(
     image: Img, password: Optional[str], hashing: bool = True
-) -> IO[bytes]:
-    # TODO read Image_pixels
-    # TODO transform pixels
-    # TODO extract_file
-    # TODO decompress
-    # TODO dectypt
-    # TODO compare Hash
-    # TODO Raise high-level Errors
+) -> Tuple[str, bytes]:
+    """
+    Decode the hidden file from an Image and validate the result if hashing is enabled.
+    :param image: The Image with the hidden file
+    :param password: The Password for decrypting the file (can be None)
+    :param hashing: bool if hashing is encoded
+    :return: Tuple[file_name, file_bytes]
+    """
+    if password is None:
+        encryption_key = DEFAULT_ENCRYPTION_KEY
+    else:
+        encryption_key = password  # type:ignore
+    rgb, alpha = encode_image_to_rgb_and_alpha_array(image)
+    n_lsb, lsb_bits = _decode_bits_from_pixels(rgb)
+    file_hash, file_name, file_bytes = extract_file_and_metadata_from_raw_bits(
+        lsb_bits, encryption_key, hashing
+    )
+    if file_hash:
+        if validate_hash(file_hash, file_bytes):
+            # TODO Raise high Level Error
+            raise NotImplementedError
     # TODO Parameter checking
-    raise NotImplementedError
+    return file_name, file_bytes
