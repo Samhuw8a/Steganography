62d9327b900af211a18e270d72878182162a1d9e
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-01-27 17:20:14 +0100

    Commenting

--- src/steganography/embed_cmd.py
+++ src/steganography/embed_cmd.py
@@ -6,17 +6,20 @@ from steganography.encode import encode_file_in_image
 
 def embed(args: Namespace) -> int:
     image_path = args.target
+    # Check if image path exists
     file = args.payload
+    # Check if file exists
     file_name = file.name
     n_lsb = args.lsb
     hashing = not args.no_hash
     encryption_key = args.password
     new_name = image_path.name
     if args.output is not None:
+        # Check if file_name is valid
         new_name = args.output
-        # ----
-        image = Image.open(image_path)
-        # TODO catch Exception
+    # ----
+    image = Image.open(image_path)
+    # TODO catch Exception ImageType and ImageMode
     with file.open("rb") as f:
         file_bytes = f.read()
         new_image = encode_file_in_image(
