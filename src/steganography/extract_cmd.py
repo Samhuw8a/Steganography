a8ff8e1254e617a25b9c7bba804bd68cad9c7753
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-01-29 17:44:42 +0100

    Added comments

--- src/steganography/extract_cmd.py
+++ src/steganography/extract_cmd.py
@@ -6,10 +6,13 @@ from PIL import Image
 
 def extract(args: Namespace) -> int:
     image_path = args.file
+    # Check if it is a file
     encryption_key = args.password
     hashing = not args.no_hash
     image = Image.open(image_path)
 
+    # Check for Imagetype and ImageMode errors
+    # Check for false user input ( ho_hash, password, etc. )
     file_name, file_bytes = decode_file_from_image(image, encryption_key, hashing)
     with open(file_name, "wb") as f:
         f.write(file_bytes)
